61c4ef45ffb6ca039acfd6827957a0a0b8c77278
acelash <firefist512@gmail.com>
2016-04-16 15:57:19 +0300

    dfssdf

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 
 exports.NODE_ENV = process.env.NODE_ENV || 'development';
 exports.PORT = Number.parseInt(process.env.PORT, 10) || 3000;
-exports.DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/koa-skeleton';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:root@localhost:5432/koa-skeleton';
 
 // If true, then Koa will trust the X-Forwarded-Host header
 // For example, use this if you're behind Cloudflare
@@ -17,10 +17,11 @@ exports.TRUST_PROXY = process.env.TRUST_PROXY === 'true';
 exports.HOSTNAME = process.env.HOSTNAME;
 if (!exports.HOSTNAME) {
   console.warn('Warn: CSRF checks are disabled since there is no HOSTNAME environment variable provided');
+  //exports.HOSTNAME = 'localhost';
 }
 
-exports.RECAPTCHA_SITEKEY = process.env.RECAPTCHA_SITEKEY;
-exports.RECAPTCHA_SITESECRET = process.env.RECAPTCHA_SITESECRET;
+exports.RECAPTCHA_SITEKEY = process.env.RECAPTCHA_SITEKEY || '6LfFEvsSAAAAAOhOS5g24zMzKzR9ox3T-wmZf0rK';
+exports.RECAPTCHA_SITESECRET = process.env.RECAPTCHA_SITESECRET || '6LfFEvsSAAAAAGHycAS-eoMgx2EirKKjU_uDffg0';
 if (!exports.RECAPTCHA_SITEKEY)
   console.warn('Warn: Recaptcha will not work since RECAPTCHA_SITEKEY is not set');
 if (!exports.RECAPTCHA_SITESECRET)
